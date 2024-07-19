a85662a67a4775c8245ecd976d1f0682cb37b395
Poonam Bhange <poonamdb27@gmail.com>
2024-07-19 12:44:50 +0530

    created A section to shows count of Total Astrologers, Number of minutes, Total customer

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatsService } from '../stats.service';
 
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrl: './stats.component.css'
+  styleUrls: ['./stats.component.css']
 })
-export class StatsComponent {
+export class StatsComponent implements OnInit {
+  stats: { totalAstrologers: number; minutesConsulted: number; happyCustomers: number } = {
+    totalAstrologers: 0,
+    minutesConsulted: 0,
+    happyCustomers: 0
+  };
 
+  constructor(private statsService: StatsService) {}
+
+  ngOnInit(): void {
+    this.statsService.getStats().subscribe(data => {
+      this.stats.totalAstrologers = data.totalAstrologers;
+      this.stats.minutesConsulted = data.minutesConsulted;
+      this.stats.happyCustomers = data.happyCustomers;
+    });
+  }
 }
